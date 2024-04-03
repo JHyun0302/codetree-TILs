bc65f0597d53f83147e3f3a37deaae3d94b35252
JHyun0302 <60764632+JHyun0302@users.noreply.github.com>
2024-04-04 06:51:42 +0900

    Title: a 부터 b 까지, Time: 90ms, Memory: 8MB, Status: Wrong Answer - CodeTree

---
+++ 240404/a 부터 b 까지/a-to-b.java
@@ -0,0 +1,29 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        StringBuilder sb = new StringBuilder();
+
+        int a = Integer.parseInt(st.nextToken());
+        int b = Integer.parseInt(st.nextToken());
+
+        sb.append(a).append(" ");
+        int i = a;
+        while(true){
+            if(i >= b){
+                break;
+            }
+            if(i % 2 == 0){
+                i += 3;
+                sb.append(i).append(" ");
+            }else{
+                i *= 2;
+                sb.append(i).append(" ");
+            }
+        }
+        System.out.print(sb.toString());
+    }
+}
