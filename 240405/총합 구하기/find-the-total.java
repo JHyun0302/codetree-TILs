dd8053399f83790325641b8558243a877d366039
JHyun0302 <60764632+JHyun0302@users.noreply.github.com>
2024-04-05 06:23:28 +0900

    Title: 총합 구하기, Time: 106ms, Memory: 8MB, Status: Passed - CodeTree

---
+++ 240405/총합 구하기/find-the-total.java
@@ -0,0 +1,21 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+
+        int a = Integer.parseInt(st.nextToken());
+        int b = Integer.parseInt(st.nextToken());
+
+        int sum = 0;
+
+        for(int i = a; i <= b; i++){
+            if(i % 6 == 0 && i % 8 != 0){
+                sum += i;
+            }
+        }
+        System.out.print(sum);
+    }
+}
