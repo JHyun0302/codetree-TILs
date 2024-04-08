ad824ff68bb99a88ce5a975b6a8cc12e6941b3d1
JHyun0302 <60764632+JHyun0302@users.noreply.github.com>
2024-04-08 18:45:32 +0900

    Title: 별표 출력하기, Time: 94ms, Memory: 8MB, Status: Passed - CodeTree

---
+++ 240408/별표 출력하기/print-star.java
@@ -0,0 +1,24 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+
+        for(int i = 0; i < n; i++){
+            for(int j = n - i - 1; j < n; j++){
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+
+        for(int i = 1; i < n; i++){
+            for(int j = i; j < n; j++){
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+
+    }
+}
