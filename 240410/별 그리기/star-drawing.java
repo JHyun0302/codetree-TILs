beaedfe7bb996053721e2f4d14fe9f88f108e19f
JHyun0302 <60764632+JHyun0302@users.noreply.github.com>
2024-04-10 10:24:50 +0900

    Title: 별 그리기, Time: 262ms, Memory: 12MB, Status: Passed - CodeTree

---
+++ 240410/별 그리기/star-drawing.java
@@ -0,0 +1,31 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+
+        for(int i = 0; i < n; i++){
+            for(int j = 0; j < n - (i +1); j++){
+                System.out.print(" ");
+            }
+            for(int k = 0; k < (2 * i) + 1; k++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+
+
+        for(int i = 0; i < n -1; i++){
+            for(int j = 0; j < i +1; j++){
+                System.out.print(" ");
+            }
+            for(int k = 0; k < (2 * n) - (2*i) -3; k++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+
+    }
+}
